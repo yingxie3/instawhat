cb24cac39f11cf6038c01a26466a5a1f58cba489
Ying Xie <yingxie3@gmail.com>
2017-05-26 14:48:54 -0700

    make keras training work

--- instakeras.py
+++ instakeras.py
@@ -4,10 +4,15 @@ import pandas as pd
 import pdb
 import numpy as np
 import tensorflow as tf 
+import keras.backend as kb
 from keras import losses
-from keras.models import Sequential
+from keras import models
 from keras.layers.core import Dense
 from keras.layers.core import Flatten
+from keras.layers import Input
+from keras.layers import Embedding
+from keras.layers import concatenate
+from keras.layers.merge import *
 from keras.layers.convolutional import Conv2D
 from keras.callbacks import TensorBoard
 from keras.optimizers import adam
@@ -16,25 +21,41 @@ from instadata import *
 
 CATEGORICAL_COLUMNS = ['product_id']
 CONTINUOUS_COLUMNS  = ['order_dow', 'order_hour_of_day', 'order_number', 'days_since_prior_order']
+ALL_COLUMNS = ['product_id', 'order_dow', 'order_hour_of_day', 'order_number', 'days_since_prior_order']
 LABEL = 'label'
 MODEL_DIR = './model'
 
 # training parameters
-TRAIN_STEPS = 1000
+TRAIN_STEPS = 100
 
 def buildModel():
-    model = Sequential()
-    model.add(Dense(100, activation='relu'))
-    model.add(Dense(50, activation='relu'))
-    model.add(Dense(2))
+    productIdInput = Input(shape=(1,), dtype='int32', name='product_id')
+    productIdEmbedding = Embedding(input_dim=50000, output_dim=4)(productIdInput)
+    productIdFlatten = Flatten()(productIdEmbedding)
 
-    model.compile(loss=losses.binary_crossentropy, optimizer=adam())
+    dowInput = Input(shape=(1,), dtype='float32', name='order_dow')
+    hourInput = Input(shape=(1,), dtype='float32', name='order_hour_of_day')
+    orderNumberInput = Input(shape=(1,), dtype='float32', name='order_number')
+    daysSinceInput = Input(shape=(1,), dtype='float32', name='days_since_prior_order')
+    x = concatenate([productIdFlatten, dowInput, hourInput, orderNumberInput, daysSinceInput])
+    '''
+    x = concatenate([productIdFlatten, kb.cast(dowInput, dtype='float32'), 
+        kb.cast(hourInput, dtype='float32'), kb.cast(orderNumberInput, dtype='float32'), daysSinceInput])
+    '''
+
+    x = Dense(100, activation='relu')(x)
+    x = Dense(50, activation='relu')(x)
+    prediction = Dense(1, activation='sigmoid')(x)
+
+    model = models.Model(inputs=[productIdInput, dowInput, hourInput, orderNumberInput, daysSinceInput], outputs=[prediction])
+    model.compile(loss=losses.binary_crossentropy, optimizer=adam(), metrics=['accuracy'])
 
     board = TensorBoard(log_dir=MODEL_DIR, histogram_freq=2, write_graph=True, write_images=False)
     board.set_model(model)
     return model, board
 
 def inputFunc(df, training=True):
+    '''
     # Creates a dictionary mapping from each continuous feature column name (k) to
     # the values of that column stored in a constant Tensor.
     continuousColumns = {k: tf.constant(df[k].values) for k in CONTINUOUS_COLUMNS}
@@ -51,20 +72,23 @@ def inputFunc(df, training=True):
     # Merges the two dictionaries into one.
     columns = dict(continuousColumns)
     columns.update(categoricalColumns)
+    '''
+    columns = {k: df[k] for k in ALL_COLUMNS}
 
     if training:
         # Converts the label column into a constant Tensor.
-        label = tf.constant(df[LABEL].values)
+        label = df[LABEL]
         # Returns the feature columns and the label.
         return columns, label
     else:
         return columns
 
-def getUniqueOrdersProducts(userId, userOrders, orderPrior):
+def getUniqueOrdersProducts(userId, orderPrior):
     # construct data suitable for training. The X would be all the features, using all
     # the products the user has ever ordered. The Y would be 0/1 indicating whether this
     # time the user has ordered the product.
-    userProducts = orderPrior.loc(lambda x: x.user_id == userId)
+
+    userProducts = orderPrior.loc[lambda x: x.user_id == userId]
     poHash = {}
     for index,entry in userProducts.iterrows():
         poHash["{}:{}".format(entry['order_id'], entry['product_id'])] = 1
@@ -97,11 +121,11 @@ def addLabels(orders, poHash, allUserOrders, allUserProducts):
 
 # training and evaluating a single user's history
 def trainForUser(model, userId, userOrders, orderPrior, orderTrain):
-    trainHash, trainOrders, trainProducts = getUniqueOrdersProducts(userId, userOrders, orderTrain)
+    trainHash, trainOrders, trainProducts = getUniqueOrdersProducts(userId, orderTrain)
     if len(trainProducts) == 0:
         return
 
-    priorHash, priorOrders, priorProducts = getUniqueOrdersProducts(userId, userOrders, orderPrior)
+    priorHash, priorOrders, priorProducts = getUniqueOrdersProducts(userId, orderPrior)
     poHash = priorHash
     poHash.update(trainHash)
 
@@ -111,12 +135,16 @@ def trainForUser(model, userId, userOrders, orderPrior, orderTrain):
     priorPO = addLabels(userOrders, poHash, priorOrders, allUserProducts)
     trainPO = addLabels(userOrders, poHash, trainOrders, allUserProducts)
 
-    model.fit(input_fn=lambda: inputFunc(priorPO), steps=TRAIN_STEPS)
-    results = model.evaluate(input_fn=lambda: inputFunc(trainPO), steps=1)
-    for key in sorted(results):
-        print("%s: %s" % (key, results[key]))
+    x,y = inputFunc(priorPO)
+    history = model.fit(x=x, y=y, batch_size=32, epochs=TRAIN_STEPS)
 
-    prediction = list(model.predict(input_fn=lambda: inputFunc(trainPO, training=False)))
+    train_x, train_y = inputFunc(trainPO)
+    loss, metrics = model.evaluate(x=train_x, y=train_y)
+
+    prediction = model.predict(x=train_x)
+    prediction.reshape((len(prediction)))
+    prediction[prediction > 0.5] = 1
+    prediction[prediction < 0.5] = 0
     print(prediction)
 
     truth = trainPO[LABEL].tolist()
@@ -145,7 +173,8 @@ def predictForUser(model, orderPrior):
 
 # We use one user's past history to predict the last order
 def singleUserRegression():
-    model = buildModel()
+    pdb.set_trace()
+    model, board = buildModel()
     orders = loadDataFile('orders')
 
     prior = loadDataFile('order_products__prior')
@@ -155,7 +184,6 @@ def singleUserRegression():
     del train['add_to_cart_order']
     del train['reordered']
 
-    pdb.set_trace()
 
     orderPrior = pd.merge(left=orders, right=prior, how='inner', on='order_id')
     orderTrain = pd.merge(left=orders, right=train, how='inner', on='order_id')
